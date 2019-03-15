17471e6e6aa425ff1d112f8bdee3feb760aea9ab
yy210014 <yy210014@users.noreply.github.com>
2019-03-15 09:29:26 +0800

    调整超神属性

--- LOL_TD/scripts/Combat/Skills/A008.lua
+++ LOL_TD/scripts/Combat/Skills/A008.lua
@@ -3,6 +3,7 @@ function skill:OnLearned()
     local unit = self.Owner
     unit.Attribute:add("物理穿透", 0.1)
     unit.Attribute:add("法术穿透", 0.1)
+    unit.Attribute:add("暴击伤害", 0.5)
     unit.Attribute:add("物理伤害加成", 0.5)
     unit.Attribute:add("法术伤害加成", 0.5)
 end
